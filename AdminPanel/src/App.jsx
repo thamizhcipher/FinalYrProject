0575ec33e6618f3f7531b9243ce42f00daaa8d36
thamizhcipher <thamizhbharathi22@gmail.com>
2025-02-07 12:23:33 +0530

    First commit

---
+++ AdminPanel/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signin from './Signin'
+import Login from './Login'
+import Dashboard from './Dashboard';
+import Home from './Home';
+import Courses from './Courses';
+import Events from './Events';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Signin />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/home' element={<Home />}></Route>
+      <Route path='/dashboard' element={<Dashboard />}></Route>
+      <Route path='/courses' element={<Courses />}></Route>
+      <Route path='/events' element={<Events />}></Route>
+      
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
---
+++ AdminPanel/src/CustomNavbar.jsx
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './CustomNavbar.css'
+function NavigationBar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home" style={{letterSpacing:"3px"}}>CONNECTOR</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+            <Nav.Link href="/courses">Courses</Nav.Link>
+            <Nav.Link href="/events">Events</Nav.Link>
+            <Nav.Link href="#">Course Recommendation</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
